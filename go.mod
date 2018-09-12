3a638365d76ed99c997bbf069537a80af623c762
Daniel Fireman <danielfireman@gmail.com>
2018-09-12 10:52:13 -0300

    Primeira versão do servidor

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/dadosjusbr/remuneracao-magistrados
+
+require (
+	github.com/kelseyhightower/envconfig v1.3.0
+	github.com/labstack/echo v3.2.1+incompatible
+	github.com/labstack/gommon v0.2.7 // indirect
+	github.com/mattn/go-colorable v0.0.9 // indirect
+	github.com/mattn/go-isatty v0.0.4 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v0.0.0-20170224212429-dcecefd839c4 // indirect
+	golang.org/x/crypto v0.0.0-20180910181607-0e37d006457b // indirect
+)
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/kelseyhightower/envconfig"
+	"github.com/labstack/echo"
+)
+
+type config struct {
+	Port int `envconfig:"PORT"`
+}
+
+func main() {
+	var conf config
+	err := envconfig.Process("remuneracao-magistrados", &conf)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	s := &http.Server{
+		Addr:         fmt.Sprintf(":%d", conf.Port),
+		ReadTimeout:  5 * time.Minute,
+		WriteTimeout: 5 * time.Minute,
+	}
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error { return c.HTML(http.StatusOK, "<html><h1>Olá!</h1></html>") })
+	e.Logger.Fatal(e.StartServer(s))
+}
