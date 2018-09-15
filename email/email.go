6cc04510b2c77fb96cbd63cd57af8d66ff751cd4
JandersonCRB <jandersonangelo@hotmail.com>
2018-09-15 17:42:45 -0300

    Lint fixes

--- email/email.go
+++ email/email.go
@@ -3,25 +3,26 @@ package email
 import (
 	"errors"
 	"fmt"
+	"net/http"
+
 	"github.com/sendgrid/sendgrid-go"
 	"github.com/sendgrid/sendgrid-go/helpers/mail"
-	"net/http"
 )
 
-//Class containing the needed methods to send an email
+// Client Class containing the needed methods to send an email
 type Client struct {
 	apiKey string
 }
 
-//Client class constructor function
+// NewClient class constructor function
 func NewClient(apiKey string) (*Client, error) {
 	if len(apiKey) <= 0 {
 		return nil, errors.New("Api Key should not be empty")
 	}
-	return &Client{apiKey }, nil
+	return &Client{apiKey}, nil
 }
 
-//Send an email and return an error if it fails
+// Send an email and return an error if it fails
 func (c *Client) Send(from, to, subject, body string) error {
 	fromMail := mail.NewEmail("", from)
 	toMail := mail.NewEmail("", to)
@@ -32,9 +33,9 @@ func (c *Client) Send(from, to, subject, body string) error {
 		return err
 	}
 	if response.StatusCode != http.StatusAccepted {
-		return fmt.Errorf("An error has ocurred while trying to send an email.\n" +
-			"Status Code: %d\n" +
-			"Body: %s\n" +
+		return fmt.Errorf("An error has ocurred while trying to send an email.\n"+
+			"Status Code: %d\n"+
+			"Body: %s\n"+
 			"Headers: %v",
 			response.StatusCode,
 			response.Body,
