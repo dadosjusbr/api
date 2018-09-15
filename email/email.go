199a02275d50095c4817582b606d1d5ed7ed66f1
JandersonCRB <jandersonangelo@hotmail.com>
2018-09-15 14:59:42 -0300

    Added documentation

--- email/email.go
+++ email/email.go
@@ -7,10 +7,12 @@ import (
 	"net/http"
 )
 
+//Class containing the needed methods to send an email
 type Client struct {
 	apiKey string
 }
 
+//Client class constructor function
 func NewClient(apiKey string) (*Client, error) {
 	if len(apiKey) <= 0 {
 		return nil, errors.New("Api Key should not be empty")
@@ -18,6 +20,7 @@ func NewClient(apiKey string) (*Client, error) {
 	return &Client{apiKey }, nil
 }
 
+//Send an email and return an error if it fails
 func (c *Client) Send(from, to, subject, body string) error {
 	fromMail := mail.NewEmail("", from)
 	toMail := mail.NewEmail("", to)
