2e308d07ac42b72768a3d643eef8de7b53fbd8d1
Tony Messias <tonyzrp@gmail.com>
2018-09-15 13:01:14 -0300

    Fix linting

--- store/client_test.go
+++ store/client_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestBuildsPCloudURL(t *testing.T) {
-	url := buildPCLoudUrl("testing", url.Values{"lorem": {"ipsum"}})
+	url := buildPCLoudURL("testing", url.Values{"lorem": {"ipsum"}})
 
 	if url != "https://api.pcloud.com/testing?lorem=ipsum" {
 		t.Error("Could not properly build the URL to pcloud")
--- store/url_builder.go
+++ store/url_builder.go
@@ -4,8 +4,8 @@ import (
 	"net/url"
 )
 
-// buildPCLoudUrl returns the full URL string formatted to consume the PCloud API.
-func buildPCLoudUrl(path string, values url.Values) string {
+// buildPCLoudURL returns the full URL string formatted to consume the PCloud API.
+func buildPCLoudURL(path string, values url.Values) string {
 	const (
 		apiScheme = "https"
 		host      = "api.pcloud.com"
