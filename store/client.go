b0646c51e9570cfa91c0c6fc18012931041ccba0
Tony Messias <tonyzrp@gmail.com>
2018-09-15 15:12:10 -0300

    Authenticates with PCloud when instiatinig the client

--- store/client.go
+++ store/client.go
@@ -1,10 +1,58 @@
 package store
 
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
 // PCloudClient represents the PCloud client instance to interact with PCLoud API.
 type PCloudClient struct {
+	Client *http.Client
+	Token  string
+}
+
+// authResponse; internal representation of the auth response. Will be used to Unmarshal the response
+type authResponse struct {
+	Auth string
+}
+
+// authenticate; sends the HTTP request to authenticate with PCloud using provided credentials.
+func authenticate(c *http.Client, username string, password string) (*http.Response, error) {
+	return c.Get(buildPCLoudURL("userinfo", url.Values{
+		"getauth":  {"1"},
+		"logout":   {"1"},
+		"username": {username},
+		"password": {password},
+	}))
 }
 
 // NewPCloudClient returns the PCloudClient to interact with PCloudAPI, or error in case using wrong credentials.
 func NewPCloudClient(username string, password string) (*PCloudClient, error) {
-	return &PCloudClient{}, nil
+	c := &http.Client{}
+
+	// We are hitting the PCloud API when to create the instance.
+	resp, err := authenticate(c, username, password)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// Converting the JSON response to bytes.
+	data, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// We are going to use this struct to Unmarshal the JSON response from PCloud.
+	jsonResponse := authResponse{}
+
+	if err := json.Unmarshal(data, &jsonResponse); err != nil {
+		return nil, err
+	}
+
+	// The instance will always have a
+	return &PCloudClient{Client: c, Token: jsonResponse.Auth}, err
 }
--- store/client_test.go
+++ store/client_test.go
@@ -1,6 +1,8 @@
 package store
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"net/url"
 	"testing"
 )
@@ -10,5 +12,24 @@ func TestBuildsPCloudURL(t *testing.T) {
 
 	if url != "https://api.pcloud.com/testing?lorem=ipsum" {
 		t.Error("Could not properly build the URL to pcloud")
+	}
+}
+
+func TestAuthenticateWithPCloud(t *testing.T) {
+	username, password := "fakeuser", "fakepass"
+
+	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		// Test request parameters
+		if req.URL.String() != "https://api.pcloud.com/userinfo?getauth=1&logout=1&username="+username+"&password="+password {
+			t.Error("Authentication is using the wrong URL.")
+		}
+		// Send response to be tested
+		rw.Write([]byte(`{"auth": "fakeToken"}`))
+	}))
+
+	resp, _ := authenticate(server.Client(), username, password)
+
+	if resp.StatusCode != 200 {
+		t.Error("Authentication failed. Probably using the wrong URL.")
 	}
 }
