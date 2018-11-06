f09d32cc354f9b8b0cc2f4db8072e475b5ec6ef5
Vinicius <viniciusaagostini@gmail.com>
2018-11-05 23:57:21 -0300

    add parsing error types

--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,8 @@
 package parser
 
-import ()
+import (
+	"strings"
+)
 
 type SheetType int
 
@@ -9,5 +11,39 @@ const (
 	XLSX
 )
 
+type ApplicationError struct {
+	message string
+}
+
+func (e *ApplicationError) Error() string {
+	return e.message
+}
+
+type DataError struct {
+	message string
+}
+
+func (e *DataError) Error() string {
+	return e.message
+}
+
+type ParsingErrors struct {
+	aplicationErrors []ApplicationError
+	dataErrors       []DataError
+}
+
+func (pe *ParsingErrors) Error() string {
+	var errorMessages []string
+
+	for _, ae := range pe.aplicationErrors {
+		errorMessages = append(errorMessages, ae.Error())
+	}
+
+	for _, de := range pe.dataErrors {
+		errorMessages = append(errorMessages, de.Error())
+	}
+
+	return strings.Join(errorMessages, ",")
+}
 
 
