1bee93dbd89b0e9e62dceb5689dab1e79a99cac0
David <david.pereira@ccc.ufcg.edu.br>
2019-09-16 14:35:13 -0300

    Ajeitando comentarios da ultima PR. Já esta fucionando para 2019 e 2018 apenas para a categoria membros ativos.

--- crawler/MPPB/crawler-mpsp.go
+++ crawler/MPPB/crawler-mpsp.go
@@ -1,16 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"flag"
-	"strings"
-	"net/http"
+	"fmt"
 	"io/ioutil"
+	"net/http"
+	"strings"
+
 	"github.com/jackdanger/collectlinks" //Da uma olhada melhor nessa library
 )
 
 const linkPrincipal = "http://www.mpsp.mp.br/portal/page/portal/Portal_da_Transparencia/Contracheque/"
-var categorias = [10]string{
+
+var categorias = []string{
 	"Membros_ativos",
 	"Membros_inativos",
 	"Servidores_ativos",
@@ -19,66 +21,77 @@ var categorias = [10]string{
 	"Pensionistas/Pensionistas_servidores",
 	"valores_colaboradores",
 	"Verbas-exec-anteriores/Verbas-exec-anteriores-Servidores",
-	"/Verbas-exec-anteriores/Verbas-exec-anteriores-Membros/Ativos_membros",
+	"Verbas-exec-anteriores/Verbas-exec-anteriores-Membros/Ativos_membros",
 	"Verbas-exec-anteriores/Verbas-exec-anteriores-Membros/Inativos_membros",
 }
 
-func main(){
-	mes := flag.Int("mes", 1234, "O mês da planilha")
-	ano := flag.Int("ano", 1234, "O ano da planilha")
-	
+func main() {
+	mes := flag.String("mes", "1234", "O mês da planilha")
+	ano := flag.Int("ano", -1, "O ano da planilha")
 	flag.Parse()
+	fmt.Printf("Mês: %s \n", *mes)
+	fmt.Printf("Ano: %d \n", *ano)
+	imprimeLinksDeUmAnoMesEspecifico(*mes, *ano)
+}
 
-	fmt.Println("Mês: ")
-	fmt.Println(*mes)
-	fmt.Println("Ano: ")
-	fmt.Println(*ano)
+func imprimeLinksDeUmAnoMesEspecifico(mes string, ano int) {
+	switch ano {
+	case 2018:
+		imprime2018(mes)
+	case 2019:
+		imprime2019(mes)
+	default:
+		fmt.Println("not implemented yet")
+	}
+}
 
-	linksDePlanilhasPorCategorias()
+func imprime2018(mes string) {
+	var url string
+	if mes == "12" {
+		url = linkPrincipal + "Membros_ativos/Tabela%20I%20membros%20ativos%20ref122018.ods"
+	} else {
+		url = linkPrincipal + "Membros_ativos/Tabela%20I%20membros%20ativos%20ref" + mes + ".ods"
+	}
+	fmt.Println(url)
+}
 
+func imprime2019(mes string) {
+	url := linkPrincipal + "/Membros_ativos/Tabela%20I%20membros%20ativos%20ref" + mes + "19.ods"
+	fmt.Println(url)
 }
 
 //Imprime os links de todas as planilhas ODS de todas as categorias
-func linksDePlanilhasPorCategorias(){
+func linksDePlanilhasPorCategorias() {
 	for _, categoria := range categorias {
-		url  := linkPrincipal + categoria
-		fmt.Println("Categoria: ", categoria )
+		url := linkPrincipal + categoria
+		fmt.Println("Categoria: ", categoria)
 		imprimeLinksDePlanilhasOds(url)
-	} 
+	}
 }
 
-
 //Imprime os links de todas as planilhas ODS de uma pagina com a url passada como parametro.
-func imprimeLinksDePlanilhasOds(url string){
+func imprimeLinksDePlanilhasOds(url string) error {
 	resp, erro := http.Get(url)
-
 	if erro != nil {
-		return
+		return erro
 	}
-
-	links := collectlinks.All(resp.Body)  
-
-	for _, link := range(links) {  
-		
+	links := collectlinks.All(resp.Body)
+	for _, link := range links {
 		if strings.HasSuffix(link, "ods") {
 			fmt.Println(link)
 		}
-
-	}  
+	}
+	return erro
 }
 
 //Imprime todo o conteudo de uma pagina com a url passada como parametro.
-func imprimeUmaPagina(url string){
+func imprimeUmaPagina(url string) error {
 	resp, erro := http.Get(url)
-
 	if erro != nil {
-		return
+		return erro
 	}
 	defer resp.Body.Close()
-
 	body, _ := ioutil.ReadAll(resp.Body)
-
 	fmt.Println(string(body))
+	return erro
 }
-
-
