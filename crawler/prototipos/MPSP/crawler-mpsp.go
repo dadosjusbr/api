f01c5d5e5fd3759878ff86768a7d7056d26ea2a2
marcosbmf <marcos.barros.filho@ccc.ufcg.edu.br>
2019-09-24 09:51:32 -0300

    Crawler para o MPSP usando goquery.

--- crawler/prototipos/MPSP/crawler-mpsp.go
+++ crawler/prototipos/MPSP/crawler-mpsp.go
@@ -3,83 +3,137 @@ package main
 import (
 	"flag"
 	"fmt"
-	"strconv"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 const linkPrincipal = "http://www.mpsp.mp.br/portal/page/portal/Portal_da_Transparencia/Contracheque/"
 
-var categorias = []string{
-	"/Membros_ativos/Tabela%20I%20membros%20ativos%20ref",
-	"Membros_inativos/Tabela%20I%20membros%20inativos%20ref",
-	"/Servidores_ativos/Tabela%20I%20%20servidores%20ativos%20ref",
-	"/servidores_inativos/Tabela%20I%20servidores%20inativos%20ref",
-	"Pensionistas/Pensionistas_membros/Benefici%C3%A1rios%20Membros%20ref%",
-	"/Pensionistas/Pensionistas_servidores/Benefici%C3%A1rios%20Servidores%20ref%",
-	//"/valores_colaboradores/Tabela%20Portal%20colaboradores%20jan%202019.pdf",  TODO: Preciso olhar essa categoria!!
-	"/Verbas-exec-anteriores/Verbas-exec-anteriores-Servidores/Tabela%20II%20servidores%20ativos%20ref",
-	"/Verbas-exec-anteriores/Verbas-exec-anteriores-Servidores/Tabela%20II%20servidores%20ativos%20ref",
-	"/Verbas-exec-anteriores/Verbas-exec-anteriores-Membros/Inativos_membros/Tabela%20II%20membros%20inativos%20ref",
+//Last piece of URL where the links are found for each category of table
+var categories = [...]string{
+	"Membros_ativos",
+	"Membros_inativos",
+	"Servidores_ativos",
+	"servidores_inativos",
+	"Pensionistas",
+	"valores_colaboradores",
+	"Verbas_exec_anteriores",
+	"Indenizacoes",
+}
+
+var meses = map[int]string{
+	1:  "janeiro",
+	2:  "fevereiro",
+	3:  "março",
+	4:  "abril",
+	5:  "maio",
+	6:  "junho",
+	7:  "julho",
+	8:  "agosto",
+	9:  "setembro",
+	10: "outubro",
+	11: "novembro",
+	12: "dezembro",
+}
+
+// Search for node descendants with the year text inside.
+func findYear(node *goquery.Selection, year string) bool {
+	for node.Size() != 0 {
+		if strings.Contains(node.Text(), year) {
+			return true
+		}
+		node = node.Children()
+	}
+	return false
+}
+
+func fetchContent(category string, month int, year int) ([]byte, error) {
+	link := fmt.Sprintf("%s%s", linkPrincipal, category)
+	res, err := http.Get(link)
+	if err != nil {
+		return nil, fmt.Errorf("Error while trying to make the Get request to (%s)", link)
+	}
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("Error while casting reader to goquery.document (%s)", link)
+	}
+	res.Body.Close()
+
+	//Select tables of interest. According to html they are inside a td.
+	sel := doc.Find("td>table")
+	var monthNodes *goquery.Selection
+	for i := range sel.Nodes {
+		node := sel.Eq(i)
+		//Try to find table with year inside, it will be right before the table with the actual links.
+		if findYear(node, fmt.Sprintf("%d", year)) {
+			//Nodes with info about the month links.
+			monthNodes = sel.Eq(i + 1).Find("a")
+			break
+		}
+	}
+	//Verify if given month exists
+	if monthNodes == nil {
+		return nil, fmt.Errorf("Error, couldn't find year %d (%s)", year, link)
+	} else if monthNodes.Size() < month-1 {
+		return nil, fmt.Errorf("Error, couldn't find month %d (%s)", month, link)
+	}
+	//Find content url
+	ssLink, ok := monthNodes.Eq(month - 1).Attr("href")
+	if !ok {
+		return nil, fmt.Errorf("Error, couldn't find href for month %d (%s)", month, link)
+	}
+
+	//Fetch spreedsheet
+	res, err = http.Get(ssLink)
+	if err != nil {
+		return nil, fmt.Errorf("Error while trying to make the Get request to (%s)", link)
+	}
+	defer res.Body.Close()
+
+	content, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("Error while trying to read content from (%s)", ssLink)
+	}
+	return content, nil
+}
+
+func saveFile(c []byte, year int, month int, category string) error {
+	//Create a new file in the cwd
+	fileName := fmt.Sprintf("%s-%02d-%d", category, month, year)
+	file, err := os.Create(fileName)
+	if err != nil {
+		return fmt.Errorf("Error creating file(%s): %q", fileName, err)
+	}
+	defer file.Close()
+
+	//Write to file
+	if _, err = file.Write(c); err != nil {
+		return fmt.Errorf("Error writing to file (%s): %q", fileName, err)
+	}
+	return nil
 }
 
 func main() {
-	mes := flag.String("mes", "", "O mês da planilha")
-	ano := flag.Int("ano", -1, "O ano da planilha")
+	month := flag.Int("mes", -1, "O mês da planilha")
+	year := flag.Int("ano", -1, "O ano da planilha")
 	flag.Parse()
 
-	msgErro, _ := validaFlags(*mes, *ano)
-	if msgErro != "" {
-		fmt.Println(msgErro)
-		return
-	}
-
-	fmt.Printf("Mês: %s \n", *mes)
-	fmt.Printf("Ano: %d \n", *ano)
-	imprimeLinksDeUmAnoMesEspecifico(*mes, *ano)
-}
-
-func validaFlags(mes string, ano int) (string, error) {
-	mesStr, erro := strconv.Atoi(mes)
-	if erro != nil {
-		return "", erro
-	}
-	if mes == "" || ano == -1 {
-		return "As flags --mes e --ano são necessarias", erro
-	} else if ano > 2019 || ano < 0 {
-		return "Não é possivel processar esse ano", erro
-	} else if mesStr > 12 || mesStr < 1 {
-		return "Mês invalido", erro
-
-	} else {
-		return "", erro
-	}
-}
-
-func imprimeLinksDeUmAnoMesEspecifico(mes string, ano int) {
-	for _, categoria := range categorias {
-		switch ano {
-		case 2018:
-			imprime2018(mes, categoria)
-		case 2019:
-			imprime2019(mes, categoria)
-		default:
-			fmt.Println("Não implementado para esse ano ainda")
+	for _, category := range categories {
+		c, err := fetchContent("Membros_ativos", *month, *year)
+		if err != nil {
+			fmt.Printf("Error while trying to fetch content %s %d-%d: %q\n", category, *month, *year, err)
+			continue
+		}
+		err = saveFile(c, *year, *month, category)
+		if err = saveFile(c, *year, *month, category); err != nil {
+			fmt.Printf("Error saving spreedsheet to file (%s %d-%d): %q\n", category, *month, *year, err)
 		}
 	}
-}
 
-func imprime2018(mes string, categoria string) {
-	var url string
-	if mes == "12" {
-		url = linkPrincipal + "Membros_ativos/Tabela%20I%20membros%20ativos%20ref122018.ods" //O mês 12 é o unico mês que foge do padrão no ano de 2018.
-	} else {
-		url = linkPrincipal + categoria + mes + ".ods"
-	}
-	fmt.Printf("Categoria: %s \n", categoria)
-	fmt.Printf("link: %s \n", url)
-}
-
-func imprime2019(mes string, categoria string) {
-	url := linkPrincipal + categoria + mes + "19.ods"
-	fmt.Printf("Categoria: %s \n", categoria)
-	fmt.Printf("link: %s \n", url)
 }
