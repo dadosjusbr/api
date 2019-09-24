634c37323cb19cfa919e66b0427e456c35ad2b25
marcosbmf <marcos.barros.filho@ccc.ufcg.edu.br>
2019-09-24 17:41:06 -0300

    Refatorando e consertando bugs na recuperaçao do link referente ao mes

--- crawler/prototipos/MPSP/crawler-mpsp.go
+++ crawler/prototipos/MPSP/crawler-mpsp.go
@@ -14,7 +14,7 @@ import (
 const linkPrincipal = "http://www.mpsp.mp.br/portal/page/portal/Portal_da_Transparencia/Contracheque/"
 
 //Last piece of URL where the links are found for each category of table
-var categories = [...]string{
+var categories = []string{
 	"Membros_ativos",
 	"Membros_inativos",
 	"Servidores_ativos",
@@ -30,7 +30,7 @@ var categories = [...]string{
 	//"Indenizacoes", Não existe
 }
 
-var meses = map[int]string{
+var months = map[int]string{
 	1:  "janeiro",
 	2:  "fevereiro",
 	3:  "março",
@@ -45,8 +45,23 @@ var meses = map[int]string{
 	12: "dezembro",
 }
 
+var fileNames = map[string]string{
+	"Membros_ativos":        "membros_ativos",
+	"Membros_inativos":      "membros_inativos",
+	"Servidores_ativos":     "servidores_ativos",
+	"servidores_inativos":   "servidores_inativos",
+	"valores_colaboradores": "colaboradores",
+	//Pensionistas separados
+	"Pensionistas/Pensionistas_membros":    "pensionistas_membros",
+	"Pensionistas/Pensionistas_servidores": "pensionistas_servidores",
+	//Verbas anteriores separadas
+	"Verbas-exec-anteriores/Verbas-exec-anteriores-Membros/Ativos_membros":   "exercicios_anteriores_membros_ativos",
+	"Verbas-exec-anteriores/Verbas-exec-anteriores-Membros/Inativos_membros": "exercicios_anteriores_membros_inativos",
+	"Verbas-exec-anteriores/Verbas-exec-anteriores-Servidores":               "exercicios_anteriores_servidores",
+}
+
 // Search for node descendants with the year text inside.
-func findYear(node *goquery.Selection, year string) bool {
+func hasYear(node *goquery.Selection, year string) bool {
 	for node.Size() != 0 {
 		if strings.Contains(node.Text(), year) {
 			return true
@@ -54,6 +69,22 @@ func findYear(node *goquery.Selection, year string) bool {
 		node = node.Children()
 	}
 	return false
+}
+
+// Search for a node in sel containing a given month in it's innerText and return it's href
+func contentLink(sel *goquery.Selection, month int) (string, error) {
+	monthStr := months[month]
+	for i := range sel.Nodes {
+		item := sel.Eq(i)
+		if strings.Contains(item.Text(), monthStr) {
+			link, ok := item.Attr("href")
+			if ok {
+				return link, nil
+			}
+			break
+		}
+	}
+	return "", fmt.Errorf("Couldn't find link for month %s", monthStr)
 }
 
 func fetchContent(category string, month int, year int) ([]byte, error) {
@@ -68,7 +99,6 @@ func fetchContent(category string, month int, year int) ([]byte, error) {
 	if err != nil {
 		return nil, fmt.Errorf("Error while casting reader to goquery.document (%s)", link)
 	}
-	res.Body.Close()
 
 	//Select tables of interest. According to html they are inside a td.
 	sel := doc.Find("td>table")
@@ -76,28 +106,28 @@ func fetchContent(category string, month int, year int) ([]byte, error) {
 	for i := range sel.Nodes {
 		node := sel.Eq(i)
 		//Try to find table with year inside, it will be right before the table with the actual links.
-		if findYear(node, fmt.Sprintf("%d", year)) {
+		if hasYear(node, fmt.Sprintf("%d", year)) {
 			//Nodes with info about the month links.
 			monthNodes = sel.Eq(i + 1).Find("a")
 			break
 		}
 	}
-	//Verify if given month exists
+
+	//Verify if there are any nodes in the search
 	if monthNodes == nil {
-		return nil, fmt.Errorf("Error, couldn't find year %d (%s)", year, link)
-	} else if monthNodes.Size() < month-1 {
-		return nil, fmt.Errorf("Error, couldn't find month %d (%s)", month, link)
-	}
-	//Find content url
-	ssLink, ok := monthNodes.Eq(month - 1).Attr("href")
-	if !ok {
-		return nil, fmt.Errorf("Error, couldn't find href for month %d (%s)", month, link)
+		return nil, fmt.Errorf("Error, couldn't find year %d", year)
 	}
 
-	//Fetch spreedsheet
+	//Find content url for a given month
+	ssLink, err := contentLink(monthNodes, month)
+	if err != nil {
+		return nil, err
+	}
+
+	//Fetch content
 	res, err = http.Get(ssLink)
 	if err != nil {
-		return nil, fmt.Errorf("Error while trying to make the Get request to (%s)", link)
+		return nil, fmt.Errorf("Error while trying to make the Get request to (%s)", ssLink)
 	}
 	defer res.Body.Close()
 
@@ -109,28 +139,8 @@ func fetchContent(category string, month int, year int) ([]byte, error) {
 }
 
 func saveFile(c []byte, year int, month int, category string) error {
-
-	//Correcting category name
-	switch category {
-	case "Pensionistas/Pensionistas_membros":
-		category = "pensionistas-membros"
-		break
-	case "Pensionistas/Pensionistas_servidores":
-		category = "pensionistas-servidores"
-		break
-	case "Verbas-exec-anteriores/Verbas-exec-anteriores-Membros/Ativos_membros":
-		category = "exec-anteriores-membros-ativos"
-		break
-	case "Verbas-exec-anteriores/Verbas-exec-anteriores-Membros/Inativos_membros":
-		category = "exec-anteriores-membros-inativos"
-		break
-	case "Verbas-exec-anteriores/Verbas-exec-anteriores-Servidores":
-		category = "exec-anteriores-servidores"
-		break
-	}
-
 	//Create a new file in the cwd
-	fileName := fmt.Sprintf("%s-%02d-%d", category, month, year)
+	fileName := fmt.Sprintf("%s-%02d-%d", fileNames[category], month, year)
 	file, err := os.Create(fileName)
 	if err != nil {
 		return fmt.Errorf("Error creating file(%s): %q", fileName, err)
@@ -157,13 +167,12 @@ func main() {
 	for _, category := range categories {
 		c, err := fetchContent(category, *month, *year)
 		if err != nil {
-			fmt.Printf("Error while trying to fetch content %s %d-%d: %q\n", category, *month, *year, err)
+			fmt.Printf("Error while trying to fetch content (%s %02d-%d): %q\n", fileNames[category], *month, *year, err)
 			continue
 		}
 		err = saveFile(c, *year, *month, category)
 		if err = saveFile(c, *year, *month, category); err != nil {
-			fmt.Printf("Error saving spreedsheet to file (%s %d-%d): %q\n", category, *month, *year, err)
+			fmt.Printf("Error saving content to file (%s %02d-%d): %q\n", fileNames[category], *month, *year, err)
 		}
 	}
-
 }
