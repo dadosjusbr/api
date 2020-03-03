de6eed18b1cfd43ec3bc0a60543228dcbd0bc02e
David <david.pereira@ccc.ufcg.edu.br>
2020-03-03 13:51:28 -0300

    fix na chamada dos dados do summary/resumo

--- ui/src/components/agency/agencyPageContainer.vue
+++ ui/src/components/agency/agencyPageContainer.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="agencyContainer">
     <div class="agencyNameContainer">
-      <h1 class="agencyName">{{ agencyName }}</h1>
+      <h1 class="agencyName">{{ agencyName.toUpperCase() }}</h1>
     </div>
     <agency-summary :agencySummary="agencySummary" />
     <graph-container />
@@ -20,13 +20,13 @@ export default {
   },
   data() {
     return {
-      agencyName: this.$route.params.agencyName.toUpperCase(),
+      agencyName: this.$route.params.agencyName,
       agencySummary: null
     };
   },
   methods: {
     async fetchData() {
-      const { data } = await this.$http.get("/orgao/resumo/a");
+      const { data } = await this.$http.get("/orgao/resumo/" + this.agencyName);
       this.agencySummary = {
         Total_Empregados: data.TotalEmployees,
         Total_Salários: data.TotalWage,
