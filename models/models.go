b1096e927c171a04a251186dd6dfa4dc7cd113c4
David <david.pereira@ccc.ufcg.edu.br>
2020-03-03 11:07:13 -0300

    fix lint

--- models/models.go
+++ models/models.go
@@ -1,5 +1,6 @@
 package models
 
+// State - Struct cotains information of a state ans its agencies
 type State struct {
 	Name      string
 	ShortName string
@@ -7,11 +8,13 @@ type State struct {
 	Agency    []AgencyBasic
 }
 
+// AgencyBasic - Basic information of a agency (name e category)
 type AgencyBasic struct {
 	Name           string
 	AgencyCategory string
 }
 
+// Employee - Represents an employee and his/her salary info
 type Employee struct {
 	Name   string
 	Wage   float64
@@ -20,6 +23,7 @@ type Employee struct {
 	Total  float64
 }
 
+// AgencySummary - Summary of an agency
 type AgencySummary struct {
 	TotalEmployees int
 	TotalWage      float64
@@ -27,11 +31,13 @@ type AgencySummary struct {
 	MaxWage        float64
 }
 
+// AgencyTotalsYear - Represents the totals of an year
 type AgencyTotalsYear struct {
 	Year        int
 	MonthTotals []MonthTotals
 }
 
+// MonthTotals - Detailed info of a month (wage, perks, other)
 type MonthTotals struct {
 	Month  int
 	Wage   float64
