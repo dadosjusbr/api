0372adce43a31e21dd2306344453fd9ad2007ae6
joeberth <joeberth.souza@ccc.ufcg.edu.br>
2020-03-13 21:33:02 -0300

    comeca a tornar a ui Material

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,13 +1,6 @@
 <template>
   <div>
     <nav-bar />
-    <div class="divisoria-colorida">
-      <div class="cor-1"></div>
-      <div class="cor-2"></div>
-      <div class="cor-3"></div>
-      <div class="cor-4"></div>
-      <div class="cor-5"></div>
-    </div>
     <router-view />
     <page-footer />
   </div>
@@ -25,4 +18,8 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+@import url("https://fonts.googleapis.com/css?family=Montserrat&display=swap");
+</style>
+
+
--- ui/src/main.js
+++ ui/src/main.js
@@ -3,9 +3,15 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import axios from "axios";
+import VueMaterial from "vue-material";
+import "vue-material/dist/vue-material.min.css";
+import "vue-material/dist/vue-material.css";
+import "vue-material/dist/vue-material.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 Vue.config.productionTip = false;
+Vue.use(VueMaterial);
+
 const base = axios.create({
   baseURL: process.env.VUE_APP_API_BASE_URL
 });
