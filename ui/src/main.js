82dcc0c8b991fb614852d3c342c819268f4ecf7e
Daniel Fireman <danielfireman@gmail.com>
2020-04-05 13:56:42 -0300

    Ligando site novo com google analytics. Fix #205

--- ui/src/main.js
+++ ui/src/main.js
@@ -4,6 +4,7 @@ import router from "./router";
 import store from "./store";
 import axios from "axios";
 import VueMaterial from "vue-material";
+import VueGtag from "vue-gtag";
 import "vue-material/dist/vue-material.min.css";
 import "vue-material/dist/vue-material.css";
 import "vue-material/dist/vue-material.min.css";
@@ -11,6 +12,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 Vue.config.productionTip = false;
 Vue.use(VueMaterial);
+Vue.use(VueGtag, {
+  config: { id: "UA-143064237-1" }
+});
 
 const base = axios.create({
   baseURL: process.env.VUE_APP_API_BASE_URL
