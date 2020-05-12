13b2caec2dd62e09c6d7e7bed680d4f766d1f1cf
davidedup <david.pereira@ccc.ufcg.edu.br>
2020-05-12 10:43:58 -0300

    spa configuration

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -1,4 +1,15 @@
+const path = require("path");
+const PrerenderSPAPlugin = require("prerender-spa-plugin");
+
 module.exports = {
   publicPath: "/",
-  productionSourceMap: false
+  productionSourceMap: false,
+  configureWebpack: {
+    plugins: [
+      new PrerenderSPAPlugin({
+        staticDir: path.join(__dirname, "dist"),
+        routes: ["/", "/sobre", "/contato"],
+      }),
+    ],
+  },
 };
