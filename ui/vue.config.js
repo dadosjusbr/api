8dd75980caa68825ad402e0cd733b0958fa759b4
Daniel Fireman <danielfireman@gmail.com>
2020-05-19 15:23:42 -0300

    Revert "Seo friendly"

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -1,15 +1,4 @@
-const path = require("path");
-const PrerenderSPAPlugin = require("prerender-spa-plugin");
-
 module.exports = {
   publicPath: "/",
-  productionSourceMap: false,
-  configureWebpack: {
-    plugins: [
-      new PrerenderSPAPlugin({
-        staticDir: path.join(__dirname, "dist"),
-        routes: ["/", "/sobre", "/contato"],
-      }),
-    ],
-  },
+  productionSourceMap: false
 };
