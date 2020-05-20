2d4e2d7b9d866276078b0d5d47526b7aa0c12c86
davidedup <david.pereira@ccc.ufcg.edu.br>
2020-05-20 15:50:38 -0300

    ajeitando erro do build

--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -3,7 +3,6 @@ import VueRouter from "vue-router";
 import Home from "@/Home.vue";
 import agencyPageContainer from "@/components/agency/agencyPageContainer.vue";
 import about from "@/components/about/about.vue";
-import contact from "@/components/contact/contact.vue";
 import agencyYearContainer from "@/components/agency-year-month/agencyYearContainer.vue";
 
 Vue.use(VueRouter);
