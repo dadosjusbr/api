2ee5ed56cb167b6b4a141e1d85ddee1f4fca5d7f
DadosjusBR Developer <64593515+dadosjusbr-dev@users.noreply.github.com>
2020-05-25 18:45:52 -0300

    Corrigindo algumas meta tags

--- ui/src/components/agency/agencyPageContainer.vue
+++ ui/src/components/agency/agencyPageContainer.vue
@@ -149,7 +149,7 @@ export default {
         { name: "twitter:card", content: "summary_large_image" },
         { name: "twitter:site", content: "@dadosjusbr" },
         { name: "twitter:creator", content: "@dadosjusbr" },
-        { name: "twitter:url", content: "https://metatags.io/" },
+        { name: "twitter:url", content: "https://dadosjusbr.org/orgao/"+ this.agencyName +"/"+ this.year +"/"+ this.month },
         { name: "twitter:title", content: "DadosJusBr" },
         {
           name: "twitter:description",
@@ -164,7 +164,7 @@ export default {
         {
           name: "twitter:image",
           content:
-            "https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png",
+            "https://dadosjusbr.org/logo.png",
         },
         {
           name: "twitter:image:alt",
