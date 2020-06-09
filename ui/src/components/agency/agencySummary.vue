c3dd6c11ff4479cfb45715aa3df72be41f8d5467
davidedup <david.pereira@ccc.ufcg.edu.br>
2020-06-09 16:37:18 -0300

    mudando bg, texto e colocando tooltips.

--- ui/src/components/agency/agencySummary.vue
+++ ui/src/components/agency/agencySummary.vue
@@ -2,25 +2,49 @@
   <div class="summary">
     <div class="wageInfoContainer">
       <div class="wageTotal">
-        <p>Total remuneração: {{ this.agencySummary.TotalRemuneration }}</p>
+        <div style="width: 95%">
+          <p>Total remuneração: {{ this.agencySummary.TotalRemuneration }}</p>
+        </div>
+        <div style="width: 5%;">
+          <md-icon
+            data-toggle="tooltip"
+            title="- Salário: valor recebido de acordo com a prestação de serviços,
+em decorrência do contrato de trabalho.
+- Remuneração: é a soma do salário mais outras vantagens (indenizações e benefícios).
+- Benefício: valores eventuais, por exemplo, auxílios alimentação, saúde, escolar..."
+            >info</md-icon
+          >
+        </div>
       </div>
       <div class="othersTotals">
         <div class="othersTotalsInfo">
-          <p>Salário máximo: {{ this.agencySummary.MaxWage }}</p>
+          <p>Maior Salário: {{ this.agencySummary.MaxWage }}</p>
           <br />
-          <p>Soma total salário: {{ this.agencySummary.TotalWage }}</p>
+          <p>Total Salários: {{ this.agencySummary.TotalWage }}</p>
         </div>
         <div class="othersTotalsInfo">
-          <p>Benefício máximo: {{ this.agencySummary.MaxPerk }}</p>
+          <p>Maior Benefício: {{ this.agencySummary.MaxPerk }}</p>
           <br />
-          <p>Soma total benefícios: {{ this.agencySummary.TotalPerks }}</p>
+          <p>Total benefícios: {{ this.agencySummary.TotalPerks }}</p>
         </div>
       </div>
     </div>
     <div class="whiteSpace"></div>
     <div class="employeeInfoContainer">
       <div class="toltalEmployees">
-        <p>Total empregados: {{ this.agencySummary.TotalEmployees }}</p>
+        <div style="width: 90%">
+          <p>Total empregados: {{ this.agencySummary.TotalEmployees }}</p>
+        </div>
+        <div style="width: 5%;">
+          <md-icon
+            data-toggle="tooltip"
+            data-placement="left" 
+            title="-Membros: 
+-Ativos:
+-Inativos:"
+            >info</md-icon
+          >
+        </div>
       </div>
       <div class="employeesClassification">
         <div class="employeeClass">
@@ -62,6 +86,8 @@ export default {
   flex-direction: column;
 }
 .wageTotal {
+  display: flex;
+  flex-direction: row;
   font-size: 17px;
   height: 33%;
   width: 100%;
@@ -78,6 +104,7 @@ export default {
   flex-direction: row;
   justify-content: center;
   align-items: center;
+  background-color: white;
 }
 
 .othersTotalsInfo {
@@ -96,6 +123,8 @@ export default {
 }
 
 .toltalEmployees {
+  display: flex;
+  flex-direction: row;
   font-size: 17px;
   order: 1;
   height: 33%;
@@ -113,6 +142,7 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+  background-color: white;
 }
 
 .summary {
@@ -121,7 +151,9 @@ export default {
   justify-content: center;
   width: 100%;
   margin-top: 3%;
+  margin-bottom: 5%;
 }
+
 .whiteSpace {
   width: 5px;
   height: 100%;
@@ -143,5 +175,8 @@ export default {
   flex-direction: column;
   justify-content: center;
   align-items: center;
+}
+
+.tooltip {
 }
 </style>
