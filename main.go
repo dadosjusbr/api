cb7dff953530fa0607bd2a3064c1d3adf42e95d9
Joellensilva <joellen.absilva@gmail.com>
2023-01-23 14:34:40 -0300

    endereçando comentários

--- main.go
+++ main.go
@@ -189,21 +189,17 @@ func getBasicInfoOfType(c echo.Context) error {
 	} else {
 		// Caso não encontremos entre as chaves, verificamos entre os valores do mapa.
 		// Isso pois, até a consolidação ser finalizada, o front consulta a api com /Eleitoral, /Trabalho, etc.
-		for i := range jurisdicao {
-			if groupName == jurisdicao[i] {
+		for _, value := range jurisdicao {
+			if groupName == value {
 				exists = true
 				break
 			}
 		}
 		// Se a jurisdição não existir no mapa, verificamos se trata-se de um estado
 		if !exists {
-			values := [27]string{"AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"}
-			for k := range values {
-				if groupName == values[k] {
-					estadual = true
-					exists = true
-					break
-				}
+			values := map[string]struct{}{"AC": {}, "AL": {}, "AP": {}, "AM": {}, "BA": {}, "CE": {}, "DF": {}, "ES": {}, "GO": {}, "MA": {}, "MT": {}, "MS": {}, "MG": {}, "PA": {}, "PB": {}, "PR": {}, "PE": {}, "PI": {}, "RJ": {}, "RN": {}, "RS": {}, "RO": {}, "RR": {}, "SC": {}, "SP": {}, "SE": {}, "TO": {}}
+			if _, estadual = values[groupName]; estadual {
+				exists = true
 			}
 		}
 		// Se o parâmetro dado não for encontrado de forma alguma, retornamos um NOT FOUND (404)
